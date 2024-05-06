6a5da78027b96d7118729b992611112d7ec2fc38
Lucasalex1993 <lta2004@hotmail.com>
2024-05-06 20:46:20 -0300

    Corrigindo

--- banco.py
+++ banco.py
@@ -1,29 +1,31 @@
 
 from flask import Flask, jsonify
 from flask_cors import CORS
-import mysql.connector
+import psycopg2
 
 app = Flask(__name__)
 # Ativar CORS para todas as rotas
 CORS(app)  
 
 # Configuração da conexão com o banco de dados
-
-conexao_banco = mysql.connector.connect(
+conexao_banco = psycopg2.connect(
     host="localhost",
-    user="root",
+    user="postgres",
     password="191069",
     database="usuarios"
 )
 
 @app.route('/api/dados', methods=['GET'])
 def obter_dados():
-    cursor = conexao_banco.cursor(dictionary=True)
+    cursor = conexao_banco.cursor()
     cursor.execute("SELECT * FROM usuario")
     resultados = cursor.fetchall()
     cursor.close()
 
-    return jsonify(resultados)
+    # Converter os resultados em um formato que possa ser convertido em JSON
+    resultados_formatados = [dict((cursor.description[i][0], valor) for i, valor in enumerate(linha)) for linha in resultados]
+
+    return jsonify(resultados_formatados)
 
 if __name__ == '__main__':
     app.run(debug=True)
