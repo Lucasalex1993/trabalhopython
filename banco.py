2b282129ccf1f142358cbf490b48976d0d0cacd1
Lucasalex1993 <lta2004@hotmail.com>
2024-05-06 21:17:19 -0300

    Editando

--- banco.py
+++ banco.py
@@ -32,7 +32,7 @@ cursor = conexao.cursor()
 # Crie a tabela
 cursor.execute(sql.SQL("""
     CREATE TABLE {} (
-        id SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY auto_increment,
         nome VARCHAR(100),
         email VARCHAR(100),
         senha VARCHAR(100),
